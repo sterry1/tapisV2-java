75ed437e779e0f8ff992bd5b6aeef1fe3e6d701c
sterry1 <sterry1@tacc.utexas.edu>
2020-06-23 14:46:59 -0500

    reorganized the deployment and build of docker image. new docker compose file relocation for project structure

--- deployment/build-metaapi.sh
+++ deployment/build-metaapi.sh
@@ -80,7 +80,7 @@ echo "         push docker hub  -  $META_IMAGE"
                # docker push "$META_IMAGE"
 echo "         tag image for our private repository  -  jenkins2.tacc.utexas.edu:5000/$META_IMAGE"
                docker tag $META_IMAGE jenkins2.tacc.utexas.edu:5000/$META_IMAGE
-               docker push jenkins2.tacc.utexas.edu:5000/$META_IMAGE
+               # docker push jenkins2.tacc.utexas.edu:5000/$META_IMAGE
 
 echo "***      "
 echo "***      rm -rf ${IMAGE_BUILD_DIR}/${WAR_NAME}"
