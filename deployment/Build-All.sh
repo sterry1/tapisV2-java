608d7a53ccaa3007f95a6132460b6b29055d17dc
sterry1 <sterry1@tacc.utexas.edu>
2020-11-10 21:21:59 -0600

    * deployment add an uber Build-All.sh swivel meta-all-3-tst.yml to handle dev and staging environments
    * tools/testing
    added and modified new http test files

--- deployment/Build-All.sh
+++ deployment/Build-All.sh
@@ -19,14 +19,12 @@ VER=$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression
 
 export TAPIS_ENV=T2dev
 export SRVC=meta
-export SRVC_API=${SRVC}wrkr
 export TAPIS_ROOT=$(pwd)
 export GIT_COMMIT=$(git log -1 --pretty=format:"%h")
 
 echo "VER: $VER"
 echo "TAPIS_ENV: $TAPIS_ENV"
 echo "SRVC: $SRVC"
-echo "SRVC_API: $SRVC_API"
 echo "TAPIS_ROOT: $TAPIS_ROOT"
 echo "GIT_COMMIT: $GIT_COMMIT"
 echo "JAVA VERSION : ";$(java -version)
@@ -37,12 +35,18 @@ echo " ***   mvn clean install -DskipTests";echo ""
 mvn clean install -DskipTests
 
 echo ""; echo ""
+cd deployment/tapis_meta_base
+docker image build -t tapis/tapis-meta-base .
+docker tag tapis/tapis-meta-base jenkins2.tacc.utexas.edu:5000/tapis/tapis-meta-base
+docker push jenkins2.tacc.utexas.edu:5000/tapis/tapis-meta-base
+
+cd $TAPIS_ROOT
 
 echo " ***   build api image and publish ";echo ""
-deployment/build-metaapi.sh
+# deployment/build-metaapi.sh
 
 echo " ***   build worker image and publish ";echo ""
-deployment/build-metawrkr.sh
+# deployment/build-metawrkr.sh
 
 
 echo " ***   Images published and ready to deploy ";echo ""
