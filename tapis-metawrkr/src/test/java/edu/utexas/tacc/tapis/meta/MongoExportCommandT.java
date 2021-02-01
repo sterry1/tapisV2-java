8685845d997e588ed75bf6850a6c72abf4f9751b
sterry1 <sterry1@tacc.utexas.edu>
2021-02-01 15:49:57 -0600

    MongoExportCommandT added tests

--- tapis-metawrkr/src/test/java/edu/utexas/tacc/tapis/meta/MongoExportCommandT.java
+++ tapis-metawrkr/src/test/java/edu/utexas/tacc/tapis/meta/MongoExportCommandT.java
@@ -68,4 +68,9 @@ mongoexport --quiet -h=aloe-dev04.tacc.utexas.edu:27019    -d=v1airr -c=rearrang
 
  mongoexport -h=aloe-dev04.tacc.utexas.edu:27019 -u=tapisadmin -p=d3f%40ult --authenticationDatabase=admin -d=v1airr -c=rearrangement  -f="cdr1,cdr2" -q='{"repertoire_id":"1841923116114776551-242ac11c-0001-012"}'
 *
+mongoexport -h=aloe-dev08.tacc.utexas.edu:27019 -u=tapisadmin -p=d3f@ult --authenticationDatabase=admin -d=v1airr -c=rearrangement -o=onejson.json -f=\"repertoire_id,locus\" -q='{\"repertoire_id\":\"1993707260355416551-242ac11c-0001-012\"}'"
+db.getCollection('metadata').find({"tenantId":"sgci"}).count()
+
+mongoexport -h=mongo-prod-agave.tacc.utexas.edu:9000 -u=adminuser -p=4b0141548e3e9e5581772ff4 --authenticationDatabase=admin -d=api  -c=metadata --type=csv -o=scgi.csv -q='{"tenantId":"sgci"}'
+
 * */
